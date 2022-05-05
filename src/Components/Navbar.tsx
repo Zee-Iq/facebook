6ebfdaaab7f85466ccc16f88ed7e2107c2dffdbb
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-05-06 00:40:46 +0200

    finished navbar with responsive rendering logic

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,10 +1,78 @@
-import { AppBar, Toolbar } from "@mui/material";
-import React from "react";
+import styled from "@emotion/styled";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  InputBase,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
+import { Box } from "@mui/system";
+import { theme } from "../theme";
+import { Mail, Notifications } from "@mui/icons-material";
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between ",
+});
+
+const Search = styled("div")(({ theme: any }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+
+const Icons = styled(Box)(({ theme:any }) => ({
+  display: "none",
+  alignItems: "center",
+  gap: "20px",
+  [theme.breakpoints.up("sm")]:{
+    display:"flex"
+  }
+}));
+const UserBox = styled(Box)(({ theme:any }) => ({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  [theme.breakpoints.up("sm")]:{
+    display:"none"
+  }
+  
+}));
 
 const Navbar = () => {
   return (
     <AppBar position="sticky">
-      <Toolbar>Navbar</Toolbar>
+      <StyledToolbar>
+        <Typography sx={{ display: { xs: "none", sm: "block" } }} variant="h6">
+          FAKEBOOK
+        </Typography>
+        <FacebookRoundedIcon sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="search..." />{" "}
+        </Search>
+        <Icons>
+          <Badge badgeContent={1} color="error">
+            <Mail  />
+          </Badge>
+          <Badge badgeContent={4} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="https://avatars.githubusercontent.com/u/86056842?v=4"
+          />
+        </Icons>
+        <UserBox>
+        <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="https://avatars.githubusercontent.com/u/86056842?v=4"
+          />
+          <Typography variant="body1">Zeeshan</Typography>
+        </UserBox>
+      </StyledToolbar>
     </AppBar>
   );
 };
