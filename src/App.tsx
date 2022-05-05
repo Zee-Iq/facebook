3499cf7fa6f1924e24e82a8fbf3880a9a8c03ff5
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-05-05 20:31:13 +0200

    Tried out custom themes for buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,77 @@
-import { Button } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import SettingsIcon from "@mui/icons-material/Settings";
+import styled from "@emotion/styled";
+const App = () => {
+  const CrimsonButton = styled(Button)({
+    backgroundColor: "crimson",
+    color: "white",
+    m: 5,
+    "&:hover": {
+      backgroundColor: "black",
+    },
+    "&:disabled": {
+      backgroundColor: "gray",
+      color: "white",
+    },
+  });
 
-function App() {
   return (
     <div>
-      <Button  ></Button>
+      <Button 
+       size="medium"
+        /* color="otherColor" */
+         variant="contained">
+        Text
+      </Button>
+      <Button
+        startIcon={<SettingsIcon fontSize="small" color="success" />}
+        variant="contained"
+      >
+        new post
+      </Button>
+      <Button disabled variant="outlined">
+        outlined
+      </Button>
+      <Typography variant="h3" component="p">
+        something
+      </Typography>
+      <Button
+        variant="contained"
+        sx={{
+          backgroundColor: "crimson",
+          color: "crimson",
+          m: 5,
+          "&:hover": {
+            backgroundColor: "black",
+          },
+          "&:disabled": {
+            backgroundColor: "gray",
+            color: "white",
+          },
+        }}
+      >
+        {" "}
+        unique{" "}
+      </Button>
+      <Button
+        variant="contained"
+        sx={{
+          backgroundColor: "crimson",
+          color: "crimson",
+          m: 5,
+          "&:hover": {
+            backgroundColor: "black",
+          },
+          "&:disabled": {
+            backgroundColor: "gray",
+            color: "white",
+          },
+        }}
+      >
+        {" "}
+        unique{" "}
+      </Button>
+      <CrimsonButton>first custom button</CrimsonButton>
     </div>
   );
 }
